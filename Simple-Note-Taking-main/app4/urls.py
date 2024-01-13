c4c49edb9654f70fc213b6beb08783f3373e69a0
Mariam guliashvili <146021619+MariamGuliashvili@users.noreply.github.com>
2024-01-13 23:59:43 +0400

    Update urls.py

--- Simple-Note-Taking-main/app4/urls.py
+++ Simple-Note-Taking-main/app4/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('add_note/', add_note , name = 'add_note'),
     path('delete_note/<int:title>', delete_note , name = 'delete_note'),
     path('details/<int:id>',note_details, name = 'note_details'),
-    path('edit/<int:id>', edit_note, name = 'edit_note'),
+    path('edit/<int:id>', edit_note, name = 'edit_note'), 
+    
 ]
